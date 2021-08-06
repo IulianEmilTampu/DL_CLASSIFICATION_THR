71bec909c9c3bd12187799c0a403efa02f335237
IulianEmilTampu <iulian.emil.tampu@liu.se>
2021-08-06 17:13:17 +0200

    Initializaed performance print function

---
+++ print_model_performance.py
@@ -0,0 +1,152 @@
+'''
+Script that given the folder to the traned and tested model, prints all the
+performance metrics.
+
+Model name (all values are macro-averages across the classes)
+
+Fold number | Sensitivity | Specificity | ROC AUC
+-------------------------------------------------
+      1     |    0.xyzt   |    0.xyzt   | 0.xyzt
+      2     |    0.xyzt   |    0.xyzt   | 0.xyzt
+-------------------------------------------------
+   mean     |    0.xyzt   |    0.xyzt   | 0.xyzt
+   std      |    0.xyzt   |    0.xyzt   | 0.xyzt
+
+
+Fold number |  Precision  |    Recall   | F1-score
+-------------------------------------------------
+      1     |    0.xyzt   |    0.xyzt   | 0.xyzt
+      2     |    0.xyzt   |    0.xyzt   | 0.xyzt
+-------------------------------------------------
+   mean     |    0.xyzt   |    0.xyzt   | 0.xyzt
+   std      |    0.xyzt   |    0.xyzt   | 0.xyzt
+
+'''
+
+
+import os
+import sys
+import cv2
+import glob
+import json
+import pickle
+import random
+import pathlib
+import argparse
+import importlib
+import numpy as np
+
+from sklearn.metrics import average_precision_score, recall_score, roc_auc_score, f1_score
+from sklearn.preprocessing import label_binarize
+
+# parser = argparse.ArgumentParser(description='Script that prints a summary of the model perfomance.')
+# parser.add_argument('-m','--model' ,required=True, help='Specify the folder where the trained and tested model is located')
+# args = parser.parse_args()
+#
+# model_folder = args.model
+#
+# # Check that model folder exists and that the test_summary.txt file is present
+# if not os.path.isdir(model_folder):
+#     raise NameError('Model not found. Given {}. Provide a valid model path.'.format(model_folder))
+#
+# if not os.path.isfile(os.path.join(model_folder, 'test_summary.txt')):
+#     raise ValueError('The test_summary.txt file is not present in the model path. Run test first.')
+
+## All good, opening test_summary.txt file
+
+model_path = '/flush/iulta54/Research/P3-THR_DL/trained_models/LightOCT_c1_isotropic_woa_lr0001_batch400'
+
+with open(os.path.join(model_path, 'test_summary.txt')) as file:
+    test_summary = json.load(file)
+    labels = label_binarize(test_summary['labels'], classes=np.unique(test_summary['labels']))
+    pred_logits = test_summary['folds_test_logits_values']
+
+n_folds = len(pred_logits)
+
+# check if we are working on a binary or multi-class classification
+binary_classification = True
+if len(labels) > 2:
+    binary_classification = False
+
+'''
+For bynary classification we can compute all the metrics:
+- Specificity
+- Precision
+- Recall (Sensitivity)
+- F1-score
+- AUC for ROC
+
+For multi-class classification we can only compute:
+- Precision
+- Recall (Sensitivity)
+- F1-score
+- AUC for ROC
+'''
+
+performance = {
+            'ROC_AUC':[],
+            'Precision': [],
+            'Recall':[],
+            'F1':[]
+                }
+if binary_classification:
+    performance['Specificity'] = []
+
+for f in range(n_folds):
+    performance["Precision"].append(average_precision_score(np.argmax(labels), np.argmax(pred_logits[f]),
+                                                    average="macro"))
+    performance["Recall"].append(recall_score(np.argmax(labels,-1), np.argmax(pred_logits[f],-1),
+                                                    average="macro"))
+    performance['ROC_AUC'].append(roc_auc_score(labels, pred_logits[f], multi_class='ovr', average='macro'))
+    performance['F1'].append(f1_score(np.argmax(labels,-1), np.argmax(pred_logits[f],-1), average='macro'))
+
+    if binary_classification:
+        tn, fp, fn, tp = confusion_matrix(np.argmax(labels,-1), np.argmax(pred_logits[f]))
+        performance['Specificity'].append(tn / (tn + fp))
+
+
+## printing
+
+print(f'{os.path.basename(model_path)}\n')
+
+if binary_classification:
+    keys = ['Specificity','Recall','Precision', 'F1', 'ROC_AUC']
+    print(f'{"Fold":^7}{keys[0]:^11}{keys[1]:^11}{keys[2]:^11}{keys[3]:^11}{keys[4]:^11}{keys[-1]:^11}')
+    for i in range(n_folds):
+        print(f'{i+1:^7}{performance[keys[0]][i]:^11.3f}{performance[keys[1]][i]:^11.3f}{performance[keys[2]][i]:^11.3f}{performance[keys[3]][i]:^11.3f}{performance[keys[-1]][i]:^11.3f}')
+    print(f'{"-"*60}')
+    print(f'{"Average":^7}{np.mean(performance[keys[0]]):^11.3f}{np.mean(performance[keys[1]]):^11.3f}{np.mean(performance[keys[2]]):^11.3f}{np.mean(performance[keys[3]]):^11.3f}{np.mean(performance[keys[4]]):^11.3f}{np.mean(performance[keys[-1]]):^11.3f}')
+    print(f'{"STD":^7}{np.std(performance[keys[0]]):^11.3f}{np.std(performance[keys[1]]):^11.3f}{np.std(performance[keys[2]]):^11.3f}{np.std(performance[keys[3]]):^11.3f}{np.std(performance[keys[4]]):^11.3f}{np.std(performance[keys[-1]]):^11.3f}')
+
+else:
+    keys = ['Recall','Precision', 'F1', 'ROC_AUC']
+
+    print(f'{"Fold":^7}{keys[0]:^11}{keys[1]:^11}{keys[2]:^11}{keys[3]:^11}{keys[-1]:^11}')
+
+    for i in range(n_folds):
+        print(f'{i+1:^7}{performance[keys[0]][i]:^11.3f}{performance[keys[1]][i]:^11.3f}{performance[keys[2]][i]:^11.3f}{performance[keys[3]][i]:^11.3f}{performance[keys[-1]][i]:^11.3f}')
+    print(f'{"-"*60}')
+    print(f'{"Average":^7}{np.mean(performance[keys[0]]):^11.3f}{np.mean(performance[keys[1]]):^11.3f}{np.mean(performance[keys[2]]):^11.3f}{np.mean(performance[keys[3]]):^11.3f}{np.mean(performance[keys[-1]]):^11.3f}')
+    print(f'{"STD":^7}{np.std(performance[keys[0]]):^11.3f}{np.std(performance[keys[1]]):^11.3f}{np.std(performance[keys[2]]):^11.3f}{np.std(performance[keys[3]]):^11.3f}{np.std(performance[keys[-1]]):^11.3f}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
