7b66843477f9cdbec0da4399653a6fbcfe9a4634
IulianEmilTampu <iulian.emil.tampu@liu.se>
2021-09-07 14:12:42 +0200

    Refined

--- configure_training.py
+++ configure_training.py
@@ -43,69 +43,72 @@ import utilities_models_tf
 
 ## parse inline parameters
 
-parser = argparse.ArgumentParser(description='Script that runs a cross-validation training for OCT 2D image classification.')
-parser.add_argument('-wd','--working_directory' ,required=False, help='Provide the Working Directory where the models_tf.py, utilities.py and utilities_models_tf.py files are.This folder will also be the one where the trained models will be saved. If not provided, the current working directory is used', default=os.getcwd())
-parser.add_argument('-df', '--dataset_folder', required=True, help='Provide the Dataset Folder where the Train and Test folders are present along with the dataset information file.')
-parser.add_argument('-mc', '--model_configuration', required=False, help='Provide the Model Configuration (LightOCT, M2, M3, ResNet50, VAE or others if implemented in the models_tf.py file).', default='LightOCT')
-parser.add_argument('-mn', '--model_name', required=False, help='Provide the Model Name. This will be used to create the folder where to save the model. If not provided, the current datetime will be used', default=datetime.now().strftime("%H:%M:%S"))
-parser.add_argument('-ct', '--classification_type', required=False, help='Provide the Classification Type. Chose between 1 (normal-vs-disease), 2 (normal-vs-enlarged-vs-shrinked) and 3 (normal-vs-all_diseases_available). If not provided, normal-vs-disease will be used.', default='c1')
-parser.add_argument('-cct', '--custom_classification_type', required=False, help='If the classification type is custom (not one of the dfefault one). If true, training test split will be generated here instead of using the already available one in the dataset folder. Note that all the custom classification arte based on the per-disease class split.', default=False)
-parser.add_argument('-f', '--folds', required=False, help='Number of folds. Default is 3', default='3')
-parser.add_argument('-l', '--loss', required=False, help='Loss to use to train the model (cce, wcce or sfce). Default is cce', default='cce')
-parser.add_argument('-lr', '--learning_rate', required=False, help='Learning rate.', default=0.001)
-parser.add_argument('-bs', '--batch_size', required=False, help='Batch size.', default=50)
-parser.add_argument('-is', '--input_size', nargs='+', required=False, help='Model input size.', default=(200,200))
-parser.add_argument('-ks', '--kernel_size', nargs='+', required=False, help='Encoder conv kernel size.', default=(5,5))
-parser.add_argument('-augment', '--augmentation', required=False, help='Specify if data augmentation is to be performed (True) or not (False)', default=True)
-parser.add_argument('-vld', '--vae_latent_dim', required=False, help='Dimension of the VAE latent space', default=128)
-parser.add_argument('-vkl', '--vae_kl_weight',required=False, help='KL weight in for the VAE loss', default=0.1)
-parser.add_argument('-vrl', '--vae_reconst_weight',required=False, help='Reconstruction weight in for the VAE loss', default=0.1)
-parser.add_argument('-v', '--verbose',required=False, help='How much to information to print while training: 0 = none, 1 = at the end of an epoch, 2 = detailed progression withing the epoch.', default=0.1)
-parser.add_argument('-ids', '--imbalance_data_strategy', required=False, help='Strategy to use to tackle imbalance data', default='weights')
-parser.add_argument('-db', '--debug', required=False, help='True if want to use a smaller portion of the dataset for debugging', default=False)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Script that runs a cross-validation training for OCT 2D image classification.')
+# parser.add_argument('-wd','--working_directory' ,required=False, help='Provide the Working Directory where the models_tf.py, utilities.py and utilities_models_tf.py files are.This folder will also be the one where the trained models will be saved. If not provided, the current working directory is used', default=os.getcwd())
+# parser.add_argument('-df', '--dataset_folder', required=True, help='Provide the Dataset Folder where the Train and Test folders are present along with the dataset information file.')
+# parser.add_argument('-tts', '--train_test_split', required=False, help='Provide the path to the train_test_split.json file specifying the test and training dataset.', default=None)
+# parser.add_argument('-mc', '--model_configuration', required=False, help='Provide the Model Configuration (LightOCT, M2, M3, ResNet50, VAE or others if implemented in the models_tf.py file).', default='LightOCT')
+# parser.add_argument('-mn', '--model_name', required=False, help='Provide the Model Name. This will be used to create the folder where to save the model. If not provided, the current datetime will be used', default=datetime.now().strftime("%H:%M:%S"))
+# parser.add_argument('-ct', '--classification_type', required=False, help='Provide the Classification Type. Chose between 1 (normal-vs-disease), 2 (normal-vs-enlarged-vs-shrinked) and 3 (normal-vs-all_diseases_available). If not provided, normal-vs-disease will be used.', default='c1')
+# parser.add_argument('-cct', '--custom_classification_type', required=False, help='If the classification type is custom (not one of the dfefault one). If true, training test split will be generated here instead of using the already available one in the dataset folder. Note that all the custom classification arte based on the per-disease class split.', default=False)
+# parser.add_argument('-f', '--folds', required=False, help='Number of folds. Default is 3', default='3')
+# parser.add_argument('-l', '--loss', required=False, help='Loss to use to train the model (cce, wcce or sfce). Default is cce', default='cce')
+# parser.add_argument('-lr', '--learning_rate', required=False, help='Learning rate.', default=0.001)
+# parser.add_argument('-bs', '--batch_size', required=False, help='Batch size.', default=50)
+# parser.add_argument('-is', '--input_size', nargs='+', required=False, help='Model input size.', default=(200,200))
+# parser.add_argument('-ks', '--kernel_size', nargs='+', required=False, help='Encoder conv kernel size.', default=(5,5))
+# parser.add_argument('-augment', '--augmentation', required=False, help='Specify if data augmentation is to be performed (True) or not (False)', default=True)
+# parser.add_argument('-vld', '--vae_latent_dim', required=False, help='Dimension of the VAE latent space', default=128)
+# parser.add_argument('-vkl', '--vae_kl_weight',required=False, help='KL weight in for the VAE loss', default=0.1)
+# parser.add_argument('-vrl', '--vae_reconst_weight',required=False, help='Reconstruction weight in for the VAE loss', default=0.1)
+# parser.add_argument('-v', '--verbose',required=False, help='How much to information to print while training: 0 = none, 1 = at the end of an epoch, 2 = detailed progression withing the epoch.', default=0.1)
+# parser.add_argument('-ids', '--imbalance_data_strategy', required=False, help='Strategy to use to tackle imbalance data', default='weights')
+# parser.add_argument('-db', '--debug', required=False, help='True if want to use a smaller portion of the dataset for debugging', default=False)
+# args = parser.parse_args()
+#
+# # parse variables
+# working_folder = args.working_directory
+# dataset_folder = args.dataset_folder
+# train_test_split = args.train_test_split
+# model_configuration = args.model_configuration
+# model_save_name = args.model_name
+# classification_type = args.classification_type
+# custom_classification = args.custom_classification_type == 'True'
+# loss = args.loss
+# learning_rate = float(args.learning_rate)
+# batch_size = int(args.batch_size)
+# input_size = [int(i) for i in args.input_size]
+# data_augmentation = args.augmentation
+# vae_latent_dim = int(args.vae_latent_dim)
+# vae_kl_weight = float(args.vae_kl_weight)
+# vae_reconst_weight = float(args.vae_reconst_weight)
+# N_FOLDS = int(args.folds)
+# verbose = int(args.verbose)
+# imbalance_data_strategy = args.imbalance_data_strategy
+# kernel_size = [int(i) for i in args.kernel_size]
+# debug = args.debug == 'True'
 
 # parse variables
-working_folder = args.working_directory
-dataset_folder = args.dataset_folder
-model_configuration = args.model_configuration
-model_save_name = args.model_name
-classification_type = args.classification_type
-custom_classification = args.custom_classification_type == 'True'
-loss = args.loss
-learning_rate = float(args.learning_rate)
-batch_size = int(args.batch_size)
-input_size = [int(i) for i in args.input_size]
-data_augmentation = args.augmentation
-vae_latent_dim = int(args.vae_latent_dim)
-vae_kl_weight = float(args.vae_kl_weight)
-vae_reconst_weight = float(args.vae_reconst_weight)
-N_FOLDS = int(args.folds)
-verbose = int(args.verbose)
-imbalance_data_strategy = args.imbalance_data_strategy
-kernel_size = [int(i) for i in args.kernel_size]
-debug = args.debug == 'True'
-
-# # parse variables
-# working_folder = '/flush/iulta54/Research/P3-THR_DL/'
-# dataset_folder = '/flush/iulta54/Research/Data/OCT/Thyroid_2019_refined_DeepLearning/2D_isotropic_TFR'
-# model_configuration = 'LightOCT'
-# model_save_name = 'LightOCT_TEST'
-# classification_type = 'c4'
-# custom_classification = True
-# loss = 'cce'
-# learning_rate = 0.0001
-# batch_size = 100
-# input_size = [200, 200]
-# data_augmentation = True
-# vae_latent_dim = 128
-# vae_kl_weight = 0.1
-# vae_reconst_weight = 0.1
-# N_FOLDS = 3
-# verbose = 2
-# imbalance_data_strategy = 'oversampling'
-# kernel_size = [5,5]
-# debug = False
+working_folder = '/flush/iulta54/Research/P3-THR_DL/'
+dataset_folder = '/flush/iulta54/Research/Data/OCT/Thyroid_2019_refined_DeepLearning/2D_isotropic_TFR'
+train_test_split = '/flush/iulta54/Research/Data/OCT/Thyroid_2019_refined_DeepLearning/2D_isotropic_TFR/train_test_split_rollback.json'
+model_configuration = 'LightOCT'
+model_save_name = 'LightOCT_rollback'
+classification_type = 'c1'
+custom_classification = False
+loss = 'cce'
+learning_rate = 0.0001
+batch_size = 100
+input_size = [200, 200]
+data_augmentation = True
+vae_latent_dim = 128
+vae_kl_weight = 0.1
+vae_reconst_weight = 0.1
+N_FOLDS = 3
+verbose = 2
+imbalance_data_strategy = 'weights'
+kernel_size = [5,5]
+debug = False
 
 # check if working folder and dataset folder exist
 if os.path.isdir(working_folder):
@@ -121,6 +124,11 @@ else:
 if not os.path.isdir(dataset_folder):
     print(f'The dataset folder provided does not exist. Input a valid one. Given {dataset_folder}')
     sys.exit()
+
+if not custom_classification:
+    # check if the train_test_split file is provided.
+    if not os.path.isfile(train_test_split):
+        raise ValueError('Custom classification is set to false, but give train_test_split file is not specified. Provide a valid one. Given {train_test_split}')
 
 if debug:
     print(f'\n{"-"*70}')
@@ -202,10 +210,10 @@ if custom_classification:
         file_names = file_names[0:10000]
 else:
     if (classification_type == 'c1' or classification_type == 'c2' or classification_type == 'c3'):
-        if os.path.isfile(os.path.join(dataset_folder, 'train_test_split.json')):
-            print(f'\nUsing default training test split (available in the train_test_split.json file)')
+        if os.path.isfile(train_test_split):
+            print(f'\nUsing default training test split (available in the train_test_split file)')
             print(f'{"Unique labels":<26s}: {classification_type_dict[classification_type]["unique_labels"]} ')
-            with open(os.path.join(dataset_folder, 'train_test_split.json')) as file:
+            with open(train_test_split) as file:
                 split = json.load(file)
                 train_val_filenames = split['training']
                 print(f'Initial training files: {len(train_val_filenames)}')
@@ -225,8 +233,8 @@ else:
 
                 # debug modeaus = [os.path.basename(i[0:i.find('c1')-1]) for i in c]
                 if debug:
-                    print('Running in debug mode - using less training data \n')
-                    random.seed(29)
+                    print('Running in debug mode - using less training data (20000) \n')
+                    # random.seed(29)
                     random.shuffle(train_val_filenames)
                     train_val_filenames = train_val_filenames[0:20000]
         else:
@@ -303,7 +311,7 @@ for f in train_val_filenames:
 
 train_val_filenames, train_val_labels, per_class_file_names = utilities.get_organized_files(train_val_filenames,
                     classification_type=classification_type,
-                    custom=True,
+                    custom= not (classification_type == 'c1' or classification_type == 'c2' or classification_type == 'c3'),
                     custom_labels=classification_type_dict[classification_type]['unique_labels'])
 
 class_weights = np.array([len(i) for i in per_class_file_names])
@@ -331,7 +339,7 @@ if imbalance_data_strategy == 'oversampling':
                 train_val_filenames.extend(per_class_file_names[idx]*int(i // n_class_samples))
                 train_val_filenames.extend(random.sample(per_class_file_names[idx], int(i % n_class_samples)))
 
-        class_weights = np.ones(n_classes)
+        class_weights = np.ones(len(per_class_file_names))
         print(f'\nUsing {imbalance_data_strategy} strategy to handle imbalance data.')
         print(f'Setting loss function to cce given the oversampling strategy')
         loss = 'cce'
@@ -377,58 +385,82 @@ for c in per_class_file_names:
 
 
 # for i, c in enumerate(classification_type_dict[classification_type]['unique_labels']):
-#     print(f'{c}')
 #     for v in per_class_unique_volumes[i]:
 #         print(v)
 
-# # 2
-# kf = KFold(n_splits=N_FOLDS)
-# per_fold_train_files = [[] for i in range(N_FOLDS)]
-# per_fold_val_files = [[] for i in range(N_FOLDS)]
-#
-# for idx1, c in enumerate(per_class_unique_volumes):
-#     # for all classes
-#     for idx, (train_volume_index, val_volume_index) in enumerate(kf.split(c)):
-#         # use the indexes of the unique volumes for split the data
-#         # training
-#         for v in train_volume_index:
-#             tr_vol = c[v]
-#             per_fold_train_files[idx].extend([f for f in train_val_filenames if tr_vol in f])
-#         # validation
-#         for v in val_volume_index:
-#             val_vol = c[v]
-#             # print(f'Fold {idx} - Validation: {val_vol}')
-#             per_fold_val_files[idx].extend([f for f in train_val_filenames if val_vol in f])
-#
-# # shuffle training files (since that there can be many files, the buffer size
-# # for the generator should be very large. By shuffling now we can reduce the
-# # buffer size).
-#
-# for c in range(N_FOLDS):
-#     random.shuffle(per_fold_train_files[c])
-#     random.shuffle(per_fold_val_files[c])
-#
-# # check that the split is valid
-# for c in range(N_FOLDS):
-#     for tr_f in per_fold_train_files[c]:
-#         if tr_f in per_fold_val_files[c]:
-#             print(f'File {os.path.basename(tr_f)} in both set for fold {c}')
-#             raise ValueError('Train validation split did not go as planned \n Some training file are in the validation set. Check implementation')
-
+N_FOLDS = 1
 
 # 2
-kf = KFold(n_splits=N_FOLDS)
-per_fold_train_files = [[] for i in range(N_FOLDS)]
-per_fold_val_files = [[] for i in range(N_FOLDS)]
+if N_FOLDS >= 2:
+    kf = KFold(n_splits=N_FOLDS)
+    per_fold_train_files = [[] for i in range(N_FOLDS)]
+    per_fold_val_files = [[] for i in range(N_FOLDS)]
 
+    for idx1, c in enumerate(per_class_unique_volumes):
+        # for all classes
+        for idx, (train_volume_index, val_volume_index) in enumerate(kf.split(c)):
+            # use the indexes of the unique volumes for split the data
+            # training
+            for v in train_volume_index:
+                tr_vol = c[v]
+                per_fold_train_files[idx].extend([f for f in train_val_filenames if tr_vol in f])
+            # validation
+            for v in val_volume_index:
+                val_vol = c[v]
+                # print(f'Fold {idx} - Validation: {val_vol}')
+                per_fold_val_files[idx].extend([f for f in train_val_filenames if val_vol in f])
 
-for idx, (tr_index, val_index) in enumerate(kf.split(train_val_filenames)):
-    per_fold_train_files[idx] = [train_val_filenames[i] for i in tr_index]
-    per_fold_val_files[idx] = [train_val_filenames[i] for i in val_index]
+    # shuffle training files (since that there can be many files, the buffer size
+    # for the generator should be very large. By shuffling now we can reduce the
+    # buffer size).
 
-# shuffle training files (since that there can be many files, the buffer size
-# for the generator should be very large. By shuffling now we can reduce the
-# buffer size).
+    for c in range(N_FOLDS):
+        random.shuffle(per_fold_train_files[c])
+        random.shuffle(per_fold_val_files[c])
+
+else:
+    # set 1000 images from each class as validation (like the testing)
+    n_images_per_class = 1000
+    per_fold_train_files = [[] for i in range(N_FOLDS)]
+    per_fold_val_files = [[] for i in range(N_FOLDS)]
+
+    random.seed(29)
+    per_class_random_files = []
+    index_of_selected_files = []
+
+    # randomly select as many volumes per class as needed to reach n_images_per_class
+    for c in per_class_unique_volumes:
+        # for this class, shuffle the volumes and get all the images untill we reach the limit
+        random.shuffle(c)
+        count = 0
+        idx = 0
+        per_class_random_files.append([])
+        while count <= n_images_per_class or idx < min_n_volumes:
+            # get all the files from that volume
+            indexes = [i for i, f in enumerate(train_val_filenames) if c[idx] in f]
+            per_class_random_files[-1].extend([train_val_filenames[i] for i in indexes])
+            index_of_selected_files.extend(indexes)
+            count += len(indexes)
+            idx += 1
+
+    # 3 get exactly n_images_per_class from each class and set it to the test set
+    for f in per_class_random_files:
+        per_fold_val_files[0].extend(random.sample(f, n_images_per_class))
+
+    # 4 get the remaining training validation files
+    per_fold_train_files[0] = [f for i, f in enumerate(train_val_filenames) if i not in index_of_selected_files]
+
+# make sure that no training file is in the test set
+for f in per_fold_train_files[0]:
+    if f in  per_fold_val_files[0]:
+        raise ValueError('Train testing split did not go as planned. Check implementation')
+
+# check that the split is valid
+for c in range(N_FOLDS):
+    for tr_f in per_fold_train_files[c]:
+        if tr_f in per_fold_val_files[c]:
+            print(f'File {os.path.basename(tr_f)} in both set for fold {c}')
+            raise ValueError('Train validation split did not go as planned \n Some training file are in the validation set. Check implementation')
 
 for c in range(N_FOLDS):
     random.shuffle(per_fold_train_files[c])
